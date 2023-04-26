b11a394b0c7549fc13bc888e441eda7519f70906
Jair <jairaceves56@gmail.com>
2023-04-25 19:47:33 -0600

    Mostramos el pokemon al dar click

--- components/pokemon/FavoriteCardPokemon.tsx
+++ components/pokemon/FavoriteCardPokemon.tsx
@@ -1,17 +1,25 @@
 import { Card } from "@nextui-org/react";
+import { useRouter } from "next/router";
 import { FC } from "react";
 
 interface Props {
     pokemonId: number
 }
 
-
 export const FavoriteCardPokemon: FC<Props> = ({ pokemonId }) => {
+
+    const router = useRouter()
+
+    const onFavoriteClicked = () => {
+        router.push(`/pokemon/${ pokemonId }`)
+    }
+
   return (
     <Card
         isHoverable
         isPressable
         css={{ padding: 10, border: 'none' }}
+        onClick={ onFavoriteClicked }
         >
         <Card.Image 
             src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${ pokemonId }.svg`}
