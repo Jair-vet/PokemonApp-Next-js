e89b8cce74d873fcda5a7de458656a5265fc85e2
Jair <jairaceves56@gmail.com>
2023-04-29 11:49:59 -0600

    Generamos las paginas si nos dan el nombre del pokemon

--- pages/name/[name].tsx
+++ pages/name/[name].tsx
@@ -133,7 +133,8 @@ export const getStaticPaths: GetStaticPaths = async (ctx) => {
       paths: pokemonNames.map( name => ({
         params: { name }
       })),
-      fallback: false,
+      // fallback: false
+      fallback: 'blocking'
     }
 }
 
@@ -142,11 +143,22 @@ export const getStaticPaths: GetStaticPaths = async (ctx) => {
 export const getStaticProps: GetStaticProps = async ({ params }) => {
   
     const { name } = params as { name: string };
+    const pokemon = await getPokemonInfo( name )
+
+    if( !pokemon ){
+      return {
+        redirect: {
+          destination: '/',
+          permanent: false
+        }
+      }
+    }
   
     return {
       props: {
-        pokemon: await getPokemonInfo( name )
-      }
+        pokemon
+      },
+      revalidate: 86400, // Para validar la pagina cada 24 hrs
     }
 }
 
